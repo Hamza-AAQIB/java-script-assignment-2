a5fcab22f0f53fd29da0cf865dab8457fb258ade
Hamza-AAQIB <122180284+Hamza-AAQIB@users.noreply.github.com>
2023-01-28 07:44:16 -0800

    i uploade java script assignment 2

---
+++ assignment 2/app.js
@@ -0,0 +1,14 @@
+var name="hamza";
+alert(name)
+var fullname="hamzaAaqib";
+alert(fullname)
+var message="HelloWorld";
+alert(message)
+var studentbiodata="hamza";
+alert(studentbiodata)
+var age="20YEARS";
+alert(age)
+var work="webDevelopment";
+alert(work)
+<----pdf3---->
+
---
+++ assignment 2/app2.js
@@ -0,0 +1,4 @@
+var visit="iHaveVisited15TimesAtThisSite"
+alert(visit)
+var order="iOrderFiveTShirts";
+alert(order)
